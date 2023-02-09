0fd68925a26cef7b83cff3388675b88cff772d5b
wasabi315 <wasabi.nvim.st@gmail.com>
2023-02-09 10:09:03 +0100

    Renamed

--- examples/zoo.ts
+++ examples/zoo.ts
@@ -2,7 +2,7 @@ import {
   Effect as E,
   State,
   Reader,
-  Raise as R,
+  Result as R,
   Flip as F,
   Defer as D,
 } from "../src/effect.ts";
@@ -17,14 +17,14 @@ function* main(): E.Effectful<number> {
   yield* S1.modify((str) => str + ", world!");
   console.log(yield* S1.get(), yield* S2.get());
   if (yield* F.flip()) {
-    yield* R.raise("error");
+    yield* R.err("error");
   }
   yield* D.defer(() => console.log("defer 2"));
   console.log(yield* S1.get(), yield* S2.get());
   yield* S2.put(yield* R1.ask());
   console.log(yield* S1.get(), yield* S2.get());
   if (yield* F.flip()) {
-    yield* R.raise("error");
+    yield* R.err("error");
   }
   console.log(yield* S1.get(), yield* S2.get());
   return yield* R1.ask();
--- src/effect/result.ts
+++ src/effect/result.ts
@@ -1,15 +1,15 @@
 import * as E from "./effect.ts";
 
-export type Result<T, E> = { ok: true; value: T } | { ok: false; error: E };
-
-class Raise extends E.Effect {
+class Err extends E.Effect {
   constructor(public error: unknown) {
     super();
   }
 }
 
 // deno-lint-ignore no-explicit-any
-export const raise = (error: unknown) => E.perform<any>(new Raise(error));
+export const err = (error: unknown) => E.perform<any>(new Err(error));
+
+export type Result<T, E> = { ok: true; value: T } | { ok: false; error: E };
 
 export function run<T>(comp: E.Effectful<T>): E.Effectful<Result<T, unknown>> {
   return E.matchWith(comp, {
@@ -20,7 +20,7 @@ export function run<T>(comp: E.Effectful<T>): E.Effectful<Result<T, unknown>> {
       return { ok: false, error };
     },
     effc(eff) {
-      if (eff instanceof Raise) {
+      if (eff instanceof Err) {
         return (_) => E.pure({ ok: false, error: eff.error });
       }
       return null;
